bb583f0d7007fc3899499ada4759d056c0e405e7
YSTey0808 <ystey0808@gmail.com>
2026-01-16 21:05:15 +0800

    Fix server error

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db"); // This pulls in your connection logic
 
-const eventRoute = require('./routes/eventRoute'); 
+const eventRoute = require("./routes/eventRoute");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test-db", async (req, res) => {
+app.get("/api/v1", async (req, res) => {
   try {
-    const result = await pool.query("SELECT NOW()");
+    const result = await db.query("SELECT NOW()");
     res.json({ success: true, time: result.rows[0].now });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
