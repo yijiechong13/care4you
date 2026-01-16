1241c00c9971c67e9c1d048776bd17c13214e151
YSTey0808 <ystey0808@gmail.com>
2026-01-16 14:29:34 +0800

    Deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const pool = require("./db"); // This pulls in your connection logic
+const db = require("./db"); // This pulls in your connection logic
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/test-db", async (req, res) => {
+app.get("/api/v1", async (req, res) => {
   try {
-    const result = await pool.query("SELECT NOW()");
+    const result = await db.query("SELECT NOW()");
     res.json({ success: true, time: result.rows[0].now });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
