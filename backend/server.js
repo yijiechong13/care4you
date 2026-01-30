967f8b30015fb4b8f48218a8be8ffe021971e010
YSTey0808 <ystey0808@gmail.com>
2026-01-30 23:58:39 +0800

    Debug

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const db = require("./db"); // This pulls in your connection logic
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const authRoute = require("./routes/authRoute");
 const eventRoute = require("./routes/eventRoute");
@@ -13,8 +15,6 @@ const userRoutes = require("./routes/userRoute");
 const announcementRoute = require("./routes/announcementRoute");
 const attendanceRoute = require("./routes/attendanceRoute");
 const translateRoute = require("./routes/translateRoute");
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/events", eventRoute);
